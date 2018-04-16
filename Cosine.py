d88796006392adf52e003432b4e8a2d1f9cf691a
francoismarquier <36259396+francoismarquier@users.noreply.github.com>
2018-04-16 14:28:40 +0200

    fonction Cosine
    calcule le cosinus de l'angle en radian

---
+++ Cosine.py
@@ -0,0 +1,9 @@
+"""
+test de management de version de programme par github
+"""
+import numpy as np
+
+angle = 45 # angle en degrés
+angle_rad = angle*np.pi/180 #deg2rad(angle)
+result = np.cos(angle_rad)
+print(result)
